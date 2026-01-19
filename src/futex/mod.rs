2d33a4409c51cf169e404948b136af11e4010de7
Fabrice <fabrice@schaub-dev.xyz>
2026-01-19 07:39:42 +0100

    remove trialing

--- src/futex/mod.rs
+++ src/futex/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(target_os = "linux")]
 pub mod linux;
 
-#[cfg(not(any(target_os = "linux",)))]
+#[cfg(not(any(target_os = "linux")))]
 compile_error!("Futex is not implemented for this operating system");
 
 pub mod bindings;
